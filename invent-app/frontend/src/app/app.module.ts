46dcbd0393d926ed5b3d939661f20d66ebe39309
pdamort <pdamort359@g.educaand.es>
2023-10-25 00:43:55 +0200

    all components

--- invent-app/frontend/src/app/app.module.ts
+++ invent-app/frontend/src/app/app.module.ts
@@ -7,12 +7,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { MaterialModule } from './material/material.module';
 import { NumberFormatPipe } from './pipe/numberformat.pipe';
+import { PackageTypeComponent } from './pages/package-type/packageType/package-type.component';
+import { LogisticsCompaniesComponent } from './pages/logistics-companies/logistics-companies.component';
+import { ItemsComponent } from './pages/items/items/items.component';
+import { UsersComponent } from './pages/users/users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NumberFormatPipe
+    NumberFormatPipe,
+    PackageTypeComponent,
+    LogisticsCompaniesComponent,
+    ItemsComponent,
+    UsersComponent,
   ],
   imports: [
     BrowserModule,
---
+++ invent-app/frontend/src/app/pages/package-type/packageType/package-type.component.ts
@@ -0,0 +1,58 @@
+import { DataSource } from '@angular/cdk/collections';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Observable, ReplaySubject } from 'rxjs';
+import { PackageType } from 'src/app/interfaces/package-type.interface';
+
+@Component({
+  selector: 'app-package-type',
+  templateUrl: './package-type.component.html',
+  styleUrls: ['./package-type.component.css']
+})
+export class PackageTypeComponent {
+
+  panelOpenState = false;
+  //packageType: PackageType[] = [];
+  displayedColumns: string[] = ['typeName', 'weight', 'form'];
+  dataSource: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // Realiza una solicitud a tu API aquí
+    this.http.get('http://localhost:3000/api/v1/package-type').subscribe((data: any) => {
+      const packageType = data;
+      this.dataSource = new PackageTypeDataSource(packageType);
+      console.log(packageType);
+    });
+  }
+
+
+  addData() {
+    // Lógica para agregar datos aquí
+  }
+
+  removeData() {
+    // Lógica para eliminar datos aquí
+  }
+
+}
+
+class PackageTypeDataSource extends DataSource<PackageType> {
+  private _dataStream = new ReplaySubject<PackageType[]>(1);
+
+  constructor(initialData: PackageType[]) {
+    super();
+    this.setData(initialData);
+  }
+
+  connect(): Observable<PackageType[]> {
+    return this._dataStream;
+  }
+
+  disconnect() {}
+
+  setData(data: PackageType[]) {
+    this._dataStream.next(data);
+  }
+}
